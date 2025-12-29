eabfffe5dfa9f58ff722faa813d3a1b86c17d2f6
iratxe-pm <iratxepm04@gmail.com>
2025-12-29 19:19:25 +0100

    chore: workflow sonarclould

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,19 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.jetbrains.kotlin.jvm) apply false
+    alias(libs.plugins.sonarcloud) // Añade esto aquí
+}
+
+// Configuración de Sonar
+sonar {
+    properties {
+        property("sonar.projectKey", "TFG-Aplication_TFG") // Lo sacas de la web de Sonar
+        property("sonar.organization", "tfg-aplication")
+        property("sonar.host.url", "https://sonarcloud.io")
+
+        property("sonar.kotlin.binaries", "**/build/classes/kotlin")
+        property("sonar.coverage.jacoco.xmlReportPaths", "**/build/reports/jacoco/**/*.xml")
+    }
 }
